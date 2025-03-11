5d27b827b7af98f4faa85bf69d2336a1b2448298
Tomas de la Torre Rando <tomistyle@uoc.edu>
2025-03-11 18:26:30 +0100

    Web terminada

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,3 +16,31 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    allowTouchMode: true,
+    autoplay: {
+        delay: 2000,
+        pauseOnMouseEnter: true,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
